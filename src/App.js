2f84327ce4584f2a6bd30efb75c2c21c58eccceb
Sammwy <sammwy.dev@gmail.com>
2021-07-23 15:30:32 -0300

    Added router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+
+import Main from "./pages/main";
+import Other from "./pages/other";
 
 function App() {
   return (
     <ChakraProvider>
-      <h1>Hello World</h1>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Main />
+          </Route>
+
+          <Route path="/other" exact>
+            <Other />
+          </Route>
+        </Switch>
+      </Router>
     </ChakraProvider>
   );
 }
---
+++ src/pages/other.jsx
@@ -0,0 +1,7 @@
+export default function Other () {
+    return (
+        <div>
+            <h1>Another Message</h1>
+        </div>
+    )
+}
