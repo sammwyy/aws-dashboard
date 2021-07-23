a303b6c33464ce2857088fb1184c362b6d4cb2c6
Sammwy <sammwy.dev@gmail.com>
2021-07-23 15:23:20 -0300

    Initial source

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <h1>Hello World</h1>
+    </ChakraProvider>
+  );
+}
+
+export default App;
