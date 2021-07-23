185193ffe1fa833326b4c7dfe79984e558206709
Sammwy <sammwy.dev@gmail.com>
2021-07-23 15:41:56 -0300

    Added Sass and create components directory

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Main from "./pages/main";
 import Other from "./pages/other";
 
+import Navbar from "./components/navbar/navbar";
+
 function App() {
   return (
     <ChakraProvider>
       <Router>
+        <Navbar />
         <Switch>
           <Route path="/" exact>
             <Main />
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,9 @@
+import styles from "./navbar.module.sass";
+
+export default function Navbar () {
+    return (
+        <div>
+            <h2 className={styles["navbar"]}>Navbar</h2>
+        </div>
+    )
+}
