54e094a7b27e88b9ef772e12aceff4ab0badd6e8
Sammwy <sammwy.dev@gmail.com>
2021-07-23 21:35:57 -0300

    Sidebar per view

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,126 +1,114 @@
 import {
-    Box,
-    Collapse,
-    Drawer,
-    DrawerContent,
-    DrawerOverlay,
-    Flex,
-    Icon,
-    useColorModeValue,
-    useDisclosure,
-  } from "@chakra-ui/react";
-  import { FaClipboardCheck, FaRss } from "react-icons/fa";
-  import { AiFillGift } from "react-icons/ai";
-  import { BsGearFill } from "react-icons/bs";
-  import { HiCode, HiCollection } from "react-icons/hi";
-  import { MdHome, MdKeyboardArrowRight } from "react-icons/md";
-  
-  export default function Sidebar () {
-    const sidebar = useDisclosure();
-    const integrations = useDisclosure();
-  
-    const NavItem = (props) => {
-      const { icon, children, ...rest } = props;
-      return (
-        <Flex
-          align="center"
-          px="4"
-          pl="4"
-          py="3"
-          cursor="pointer"
-          color={useColorModeValue("inherit", "gray.400")}
-          _hover={{
-            bg: useColorModeValue("gray.100", "gray.900"),
-            color: useColorModeValue("gray.900", "gray.200"),
+  Box,
+  Drawer,
+  DrawerContent,
+  DrawerOverlay,
+  Flex,
+  Icon,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+const NavItem = (props) => {
+  const { icon, children, ...rest } = props;
+  return (
+    <Flex
+      align="center"
+      px="4"
+      pl="4"
+      py="3"
+      cursor="pointer"
+      color={useColorModeValue("inherit", "gray.400")}
+      _hover={{
+        bg: useColorModeValue("gray.100", "gray.900"),
+        color: useColorModeValue("gray.900", "gray.200"),
+      }}
+      role="group"
+      fontWeight="semibold"
+      transition=".15s ease"
+      {...rest}
+    >
+      {icon && (
+        <Icon
+          mr="2"
+          boxSize="4"
+          _groupHover={{
+            color: "gray.600",
           }}
-          role="group"
-          fontWeight="semibold"
-          transition=".15s ease"
-          {...rest}
-        >
-          {icon && (
-            <Icon
-              mr="2"
-              boxSize="4"
-              _groupHover={{
-                color: "gray.600",
-              }}
-              as={icon}
-            />
-          )}
-          {children}
-        </Flex>
-      );
-    };
-  
-    const SidebarContent = (props) => (
-      <Box
-        as="nav"
-        top="0"
-        left="0"
-        h="full"
-        pb="10"
-        overflowX="hidden"
-        overflowY="auto"
-        bg={useColorModeValue("white", "gray.800")}
-        w="60"
-        {...props}
-      >
-        <Flex
-          direction="column"
-          as="nav"
-          fontSize="sm"
-          color="gray.600"
-          aria-label="Main Navigation"
-        >
-          <NavItem icon={MdHome}>Home</NavItem>
-          <NavItem icon={FaRss}>Articles</NavItem>
-          <NavItem icon={HiCollection}>Collections</NavItem>
-          <NavItem icon={FaClipboardCheck}>Checklists</NavItem>
-          <NavItem icon={HiCode} onClick={integrations.onToggle}>
-            Integrations
-            <Icon
-              as={MdKeyboardArrowRight}
-              ml="auto"
-              transform={integrations.isOpen && "rotate(90deg)"}
-            />
-          </NavItem>
-          <Collapse in={integrations.isOpen}>
-            <NavItem pl="12" py="2">
-              Shopify
-            </NavItem>
-            <NavItem pl="12" py="2">
-              Slack
-            </NavItem>
-            <NavItem pl="12" py="2">
-              Zapier
-            </NavItem>
-          </Collapse>
-          <NavItem icon={AiFillGift}>Changelog</NavItem>
-          <NavItem icon={BsGearFill}>Settings</NavItem>
-        </Flex>
-      </Box>
-    );
-    return (
-      <Box
-        as="section"
-        bg={useColorModeValue("gray.50", "gray.700")}
-        minH="100vh"
-        width="200px"
-        marginRight="50px"
-        display="inline-flex"
-      >
-        <SidebarContent display={{ base: "none", md: "unset" }} />
-        <Drawer
-          isOpen={sidebar.isOpen}
-          onClose={sidebar.onClose}
-          placement="left"
-        >
-          <DrawerOverlay />
-          <DrawerContent>
-            <SidebarContent w="full" borderRight="none" />
-          </DrawerContent>
-        </Drawer>
-      </Box>
-    );
+          as={icon}
+        />
+      )}
+      {children}
+    </Flex>
+  );
+};
+
+const getComponentsFromContent = (content = []) => {
+  const result = [];
+
+  for (let item of content) {
+    result.push(
+      <NavItem to={item.url} icon={item.icon}>{item.content}</NavItem>
+    )
   }
+
+  return result;
+};
+
+const SidebarContent = (props) => {
+  const components = getComponentsFromContent(props.content);
+
+  return (
+    <Box
+      as="nav"
+      top="0"
+      left="0"
+      h="full"
+      pb="10"
+      overflowX="hidden"
+      overflowY="auto"
+      bg={useColorModeValue("white", "gray.800")}
+      w="60"
+      {...props}
+    >
+      <Flex
+        direction="column"
+        as="nav"
+        fontSize="sm"
+        color="gray.600"
+        aria-label="Main Navigation"
+      >
+        {components}
+      </Flex>
+    </Box>
+  );
+};
+
+export default function Sidebar({ content }) {
+  const sidebar = useDisclosure();
+  return (
+    <Box
+      as="section"
+      bg={useColorModeValue("gray.50", "gray.700")}
+      minH="100vh"
+      width="200px"
+      marginRight="50px"
+      display="inline-flex"
+    >
+      <SidebarContent
+        content={content}
+        display={{ base: "none", md: "unset" }}
+      />
+      <Drawer
+        isOpen={sidebar.isOpen}
+        onClose={sidebar.onClose}
+        placement="left"
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <SidebarContent w="full" borderRight="none" />
+        </DrawerContent>
+      </Drawer>
+    </Box>
+  );
+}
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,11 +1,24 @@
 import { useEffect } from "react"
+import { MdHome } from "react-icons/md";
+
+import { AiFillGift } from "react-icons/ai";
+import { BsGearFill } from "react-icons/bs";
+import { HiCollection } from "react-icons/hi";
 
 export default function Main ({ setSidebarContent }) {
     
+    /* Sidebar */
     useEffect(() => {
-        setSidebarContent({});
+        setSidebarContent([
+            { content: "Home", icon: MdHome, url: "/" },
+            { content: "API", icon: HiCollection, url: "/" },
+            { content: "Credits", icon: AiFillGift, url: "/" },
+            { content: "Settings", icon: BsGearFill, url: "/" }
+        ]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    /* View content */
     return (
         <div>
             <h1>Hello World</h1>
